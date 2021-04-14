d4599be0c48eac785b7edc2ad4238359b1e8b337
TomasSchlogl <thomas537@seznam.cz>
2021-04-14 17:54:44 +0200

    tvoreni responzivnich fontsizes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+  createMuiTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+  withStyles,
+} from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import styled from "styled-components";
 
+let theme = createMuiTheme();
+
+theme.typography.h1 = {
+  fontSize: "5rem",
+  letterSpacing: "-2px",
+  fontWeight: "300",
+  [theme.breakpoints.down("md")]: {
+    fontSize: "2.4rem",
+    letterSpacing: "-0.5px",
+  },
+};
+theme.typography.subtitle1 = {
+  fontSize: "1.5rem",
+  [theme.breakpoints.down("md")]: {
+    fontSize: "1.25rem",
+  },
+};
+theme.typography.subtitle2 = {
+  fontSize: "1.25rem",
+  [theme.breakpoints.down("md")]: {
+    fontSize: "1rem",
+  },
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <StyledTypography variant="h1">Responsive h1</StyledTypography>
+        <Typography variant="h2">Responsive h2</Typography>
+        <Typography variant="subtitle1">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat
+          mollitia incidunt voluptatum nobis dolores assumenda.
+        </Typography>
+      </ThemeProvider>
     </div>
   );
 }
 
 export default App;
+
+const StyledTypography = styled(Typography)`
+  color: red;
+`;
